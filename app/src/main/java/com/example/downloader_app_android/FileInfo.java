962ba14a1af8e74ed405f0b9c06cfb50c2171581
bajdaprzemyslaw <przemek.android20@gmail.com>
2021-05-26 21:37:59 +0200

    add FileInfo class and Internet permission to androidManifest

---
+++ app/src/main/java/com/example/downloader_app_android/FileInfo.java
@@ -0,0 +1,23 @@
+package com.example.downloader_app_android;
+
+public class FileInfo {
+
+    private int fileSize;
+    private String fileType;
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+}
